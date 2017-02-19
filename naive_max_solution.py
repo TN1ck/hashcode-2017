7626df357fadba95ccc76b62e17ce02442401f33
marius <marius.liwotto@gmail.com>
2017-02-19 19:33:36 +0100

    add another server into pools

--- naive_max_solution.py
+++ naive_max_solution.py
@@ -1,4 +1,7 @@
-from functools import reduce, zip
+from functools import reduce
+import random
+from main import parse_file
+
 def sort_servers_into_pools(server_list, num_pools):
     large_servers_first = list(reversed(sorted(server_list)))
     pools = []
@@ -7,7 +10,6 @@ def sort_servers_into_pools(server_list, num_pools):
     for i in range(len(large_servers_first)):
         pools[i % num_pools].append(large_servers_first[i])
     return pools
-
 
 
 def allocate_servers_to_rows(pools, rows):
@@ -26,7 +28,41 @@ def allocate_servers_to_rows(pools, rows):
                     break
 
 
+def sum_capacity(pool):
+    return sum([s['capacity'] for s in pool])
 
-while not pools_empty(pools):
 
-print(sort_servers_into_pools([1,6,6,4,8,0,1,8,7,5,8,8,6,18,20,13,4], 3))
+def update_pools_by_count(pools):
+    pools = sorted(pools, key=lambda p: len(p), reverse=True)
+    pool1 = pools[0]
+    pool2 = pools[-1]
+    min_server = min(pool1, key=lambda s: s['capacity'])
+    pool2 += [min_server] 
+    pool1.remove(min_server)
+    return pools
+
+
+def update_pools_by_capacity(pools):
+    pools = sorted(pools, key=lambda p: sum_capacity(p), reverse=True)
+    pool1 = pools[0]
+    pool2 = pools[-1]
+    min_server = min(pool1, key=lambda s: s['capacity'])
+    pool2 += [min_server]
+    pool1.remove(min_server)
+    return pools
+
+
+def sort_servers_into_pools_greedy(server_list, num_pools):
+    random.shuffle(server_list)
+    pools = [[] for i in range(num_pools)]
+    for i in range(len(server_list)):
+        pools[i % num_pools].append(server_list[i])
+    i = 0
+    while i < 10:
+        pools = update_pools_by_capacity(pools)
+        pools = update_pools_by_count(pools)
+        i += 1
+    return pools
+
+data_struct = parse_file('./testfile')
+sort_servers_into_pools_2(data_struct['servers'], data_struct['pools'])
