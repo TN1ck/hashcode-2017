9d080d53e25007f5a77f4e042abc4bb9cb93d090
Tom Nick <tomwanick@gmail.com>
2017-02-19 19:38:08 +0100

    add score method

--- main.py
+++ main.py
@@ -57,6 +57,44 @@ def parse_file(file_path):
             'pools': pools,
             'servers': parsed_servers_to_be_allocated
         }
+    
+def server_does_not_exist(server, list):
+    result = [s for s in list if s['index'] == server['index']]
+    return len(result) == 0
+
+def score (row_allocation, pools):
+    row_shutdown_result = []
+    for row in row_allocation:
+        pools_capacity = []
+        for pool in pools:
+            pool_with_outages = [server for server in pool if server_does_not_exist(server, row)]
+            pool_capacity = sum([server['capacity'] for server in pool_with_outages])
+            pools_capacity.append(pool_capacity)
+        row_shutdown_result.append(pool_capacity)
+    return row_shutdown_result
+
+print('score', score(
+    [
+        [
+            {'size': 1, 'capacity': 5, 'index': 2},
+            {'size': 2, 'capacity': 2, 'index': 4}
+        ],
+        [
+            {'size': 2, 'capacity': 4, 'index': 1},
+            {'size': 2, 'capacity': 4, 'index': 3},
+        ]
+    ],
+    [
+        [
+            {'size': 1, 'capacity': 5, 'index': 2},
+            {'size': 2, 'capacity': 4, 'index': 3}
+        ],
+        [
+            {'size': 2, 'capacity': 4, 'index': 1},
+            {'size': 2, 'capacity': 2, 'index': 4}
+        ]
+    ]
+))
 
 def __main__():
     print('HASHCODE 2015\n')
