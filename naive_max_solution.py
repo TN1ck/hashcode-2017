ba3d77c4b432f3db3069f6e109a9d7922c122415
conchulio <waltersheridan@gmail.com>
2017-02-19 17:52:08 +0100

    Uncomment crap

--- naive_max_solution.py
+++ naive_max_solution.py
@@ -8,12 +8,13 @@ def sort_servers_into_pools(server_list, num_pools):
         pools[i % num_pools].append(large_servers_first[i])
     return pools
 
-def pools_empty(pools):
-    return reduce(lambda cum, x: cum and len(x)==0, pools, true) 
-
-def allocate_servers_to_rows(pools, rows):
-
-    last_pool_used = 0
-    while not pools_empty(pools)
-
-print(sort_servers_into_pools([1,6,6,4,8,0,1,8,7,5,8,8,6,18,20,13,4], 3))
+# def pools_empty(pools):
+#     return reduce(lambda cum, x: cum and len(x)==0, pools, true)
+#
+# def allocate_servers_to_rows(pools, rows):
+#
+#     last_pool_used = 0
+# while not pools_empty(pools):
+#
+#
+# print(sort_servers_into_pools([1,6,6,4,8,0,1,8,7,5,8,8,6,18,20,13,4], 3))
