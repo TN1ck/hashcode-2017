e7a3fb4adeedd4f42b682048fe2c95575cb5e479
TN1ck <tomwanick@gmail.com>
2017-02-23 19:36:45 +0100

    parse method done

--- main.py
+++ main.py
@@ -13,40 +13,93 @@
 import sys
 import numpy as np
 
-class Pizza(object):
-    """A pizza about to be sliced"""
 
-    def __init__(self, rows, cols, min_num_ingridients, max_slice_size):
-        self.rows = rows
-        self.cols = cols
-        self.min_num_ingridients = min_num_ingridients
-        self.max_slice_size = max_slice_size
-        self.avail_grid = np.ones((rows, cols))
-        self.ingridients = np.zeros((rows, cols))
-        self.slices = []
+class Video(object):
+    """A video to be cached"""
+
+    def __init__(self, size):
+        self.size = size
 
     def __str__(self):
-        print('TODO')
+        return 'size({})'.format(self.size)
 
-    def solve(self):
-        """Solve this problem"""
-        print('TODO')
-        return
+    def __repr__(self):
+        return self.__str__()
 
+class Endpoint(object):
+
+    def __init__(self, latency_to_datacenter, cache_servers_with_latency):
+        self.latency_to_datacenter = latency_to_datacenter
+        self.cache_servers_with_latency = cache_servers_with_latency
+
+    def __str__(self):
+        return 'latency({}) caches({})'\
+            .format(self.latency_to_datacenter, len(self.cache_servers_with_latency))
+
+    def __repr__(self):
+        return self.__str__()
+
+class CacheServer(object):
+
+    def __init__(self, size):
+        self.size = size
+
+class Request(object):
+
+    def __init__(self, video_id, endpoint_id, number_of_requests):
+        self.video_id = video_id
+        self.endpoint_id = endpoint_id
+        self.number_of_requests = number_of_requests
+
+    def __str__(self):
+        return 'video({}) endpoint({}) requests({})'\
+            .format(self.video_id, self.endpoint_id, self.number_of_requests)
+
+    def __repr__(self):
+        return self.__str__()
 
 def read_file(file_path):
     """Read input file"""
     with open(file_path, 'r') as f:
         lines = [line.rstrip('\n') for line in f]
         header = lines[0]
-        [rows, cols, min_num_ingridients, max_slice_size] = [int(x) for x in header.split(' ')]
-        pizza = Pizza(rows, cols, min_num_ingridients, max_slice_size)
-        # read ingridients
-        for i, line in enumerate(lines[1:]):
-            for j in range(cols):
-                if line[j] == 'T':
-                    pizza.ingridients[i, j] = 1
-        return pizza
+        [videos, endpoints, requests, cache_servers, cache_server_capacity] = [int(x) for x in header.split(' ')]
+        print('videos {}\n endpoints {} requests {} cache_servers {} cache_server_capacity {}'.format(
+            videos, endpoints, requests, cache_servers, cache_server_capacity
+        ))
+
+        video_sizes = lines[1]
+        video_list = [Video(int(x)) for x in video_sizes.split(' ')]
+
+        endpoint_list = []
+        # keeps track of the last index, which is needed for the video parsing
+        current_index = 2
+        for i in range(endpoints):
+            # we start from the second line
+            current_endpoint = lines[current_index]
+            [latency_to_datacenter, number_of_connected_caches] = [int(x) for x in current_endpoint.split(' ')]
+            current_index += 1
+            connected_caches = []
+            for connected_cache_line in lines[current_index:(current_index + number_of_connected_caches)]:
+                [cache_id, latency] = [int(x) for x in connected_cache_line.split(' ')]
+                connected_caches.append({
+                    'cache_id': cache_id,
+                    'latency': latency
+                })
+                current_index += 1
+            endpoint_list.append(Endpoint(latency_to_datacenter, connected_caches))
+
+        request_list = []
+        for request_line in lines[current_index:]:
+            [video_id, endpoint_id, number_of_requests] = [int(x) for x in request_line.split(' ')]
+            request_list.append(Request(video_id, endpoint_id, number_of_requests))
+
+        return {
+            'videos': video_list,
+            'endpoints': endpoint_list,
+            'requests': request_list,
+            'cache_server_capacity': cache_server_capacity
+        }
 
 def write_file(pizza, filename):
     """Write output file."""
@@ -73,4 +126,6 @@ if __name__ == '__main__':
     print('HASHCODE - TU_DUDES')
     print('running python {}'.format(sys.version_info.major))
     # main()
+    result = read_file('kittens.in')
+    print(result)
 
