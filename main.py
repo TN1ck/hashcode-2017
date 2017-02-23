52f7240e22e250977863d1484a556d5bb73e18b7
TN1ck <tomwanick@gmail.com>
2017-02-23 14:57:22 +0100

    chore: move old code int testphase, add template file

--- main.py
+++ main.py
@@ -1,119 +1,76 @@
-import naive_max_solution
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Google Hash Code 2016 - Qualification
+#
+# © 2017 Team "TU_DUDES"
+#
+# Version: 0.1
+#
 
-def parse_file(file_path):
+"""Google Hash Code 2017"""
+
+import sys
+import numpy as np
+
+class Pizza(object):
+    """A pizza about to be sliced"""
+
+    def __init__(self, rows, cols, min_num_ingridients, max_slice_size):
+        self.rows = rows
+        self.cols = cols
+        self.min_num_ingridients = min_num_ingridients
+        self.max_slice_size = max_slice_size
+        self.avail_grid = np.ones((rows, cols))
+        self.ingridients = np.zeros((rows, cols))
+        self.slices = []
+
+    def __str__(self):
+        print('TODO')
+
+    def solve(self):
+        """Solve this problem"""
+        print('TODO')
+        return
+
+
+def read_file(file_path):
+    """Read input file"""
     with open(file_path, 'r') as f:
         lines = [line.rstrip('\n') for line in f]
         header = lines[0]
-        [rows, slots, unavailable_slots, pools, servers] = [int(x) for x in header.split(' ')]
-        print('rows {}, slots {}, unavailable_slots {}, pools {}, servers {}'.format(
-            rows, slots, unavailable_slots, pools, servers
-        ))
-        unavailable_slots_lines = lines[1:(1 + unavailable_slots)]
-        parsed_unavailable_slots = []
-        for line in unavailable_slots_lines:
-            (row, slot) = [int(x) for x in line.split(' ')]
-            parsed_unavailable_slots.append({'row': row, 'slot': slot})
-        print('parsed unavailable slots', parsed_unavailable_slots)
+        [rows, cols, min_num_ingridients, max_slice_size] = [int(x) for x in header.split(' ')]
+        pizza = Pizza(rows, cols, min_num_ingridients, max_slice_size)
+        # read ingridients
+        for i, line in enumerate(lines[1:]):
+            for j in range(cols):
+                if line[j] == 'T':
+                    pizza.ingridients[i, j] = 1
+        return pizza
 
-        parsed_rows = []
-        for _ in range(rows):
-            # [1, 2, 3, 4, 5]
-            parsed_rows.append([x for x in range(slots)])
-
-        for unavailable_slot in parsed_unavailable_slots:
-            # [1, False, 3, 4, 5]
-            parsed_rows[unavailable_slot['row']][unavailable_slot['slot']] = False
-
-        grouped_rows = []
-        for row in parsed_rows:
-            # [4, 1]
-            new_row = []
-            current = 0
-            for slot in row:
-                if slot is False and current > 0:
-                    new_row.append(current)
-                    current = 0
-                elif slot is not False:
-                    current += 1
-            if current > 0:
-                new_row.append(current)
-            grouped_rows.append(new_row)
-        print('grouped rows', grouped_rows)
-
-        servers_to_be_allocated_lines = lines[(1 + unavailable_slots):]
-        parsed_servers_to_be_allocated = []
-        server_index = 0
-        for line in servers_to_be_allocated_lines:
-            server_index += 1
-            (size, capacity) = ([int(x) for x in line.split(' ')])
-            parsed_servers_to_be_allocated.append({
-                'size': size,
-                'capacity': capacity,
-                'index': server_index
-            })
-        print('parsed servers', parsed_servers_to_be_allocated)
-        return {
-            'rows': grouped_rows,
-            'slots': slots,
-            'unavailable_slots': parsed_unavailable_slots,
-            'pools': pools,
-            'servers': parsed_servers_to_be_allocated
-        }
-    
-def server_does_not_exist(server, list):
-    result = [s for s in list if s['index'] == server['index']]
-    return len(result) == 0
-
-def score (row_allocation, pools):
-    row_shutdown_result = []
-    for row in row_allocation:
-        pools_capacity = []
-        for pool in pools:
-            pool_with_outages = [server for server in pool if server_does_not_exist(server, row)]
-            pool_capacity = sum([server['capacity'] for server in pool_with_outages])
-            pools_capacity.append(pool_capacity)
-        row_shutdown_result.append(pool_capacity)
-    return row_shutdown_result
-
-print('score', score(
-    [
-        [
-            {'size': 1, 'capacity': 5, 'index': 2},
-            {'size': 2, 'capacity': 2, 'index': 4}
-        ],
-        [
-            {'size': 2, 'capacity': 4, 'index': 1},
-            {'size': 2, 'capacity': 4, 'index': 3},
-        ]
-    ],
-    [
-        [
-            {'size': 1, 'capacity': 5, 'index': 2},
-            {'size': 2, 'capacity': 4, 'index': 3}
-        ],
-        [
-            {'size': 2, 'capacity': 4, 'index': 1},
-            {'size': 2, 'capacity': 2, 'index': 4}
-        ]
-    ]
-))
-
-def get_solution(parsed_data_structure):
-    max_servers = []
-    for server in parsed_data_structure['servers']:
-        max_servers.append([server['size'], server['capacity']])
-    pool_allocation = naive_max_solution.sort_servers_into_pools(max_servers, parsed_data_structure['pools'])
-    row_allocation = naive_max_solution.allocate_servers_to_rows(pool_allocation, parsed_data_structure['rows'])
-    return row_allocation
-
-def __main__():
-    print('HASHCODE 2015\n')
-    parsed_data_structure = parse_file('./testfile')
-    print('parsed file', parsed_data_structure)
-    final_allocation = get_solution(parsed_data_structure)
-    print("final_allocation", final_allocation)
+def write_file(pizza, filename):
+    """Write output file."""
+    with open(filename, 'w') as file_out:
+        file_out.write('{}\n'.format(len(pizza.slices)))
+        for cur_slice in pizza.slices:
+            [start_row, end_row, start_col, end_col] = cur_slice
+            file_out.write('{} {} {} {}\n'.format(start_row, start_col, end_row-1, end_col-1))
 
 
+def main():
+    """Main function."""
+
+    if len(sys.argv) < 3:
+        sys.exit('Syntax: %s <filename> <output>' % sys.argv[0])
+
+    # read data
+    pizza = read_file(sys.argv[1])
+    # write output file
+    write_file(pizza, sys.argv[2])
 
 
-__main__()
+if __name__ == '__main__':
+    print('HASHCODE - TU_DUDES')
+    print('running python {}'.format(sys.version_info.major))
+    # main()
+
