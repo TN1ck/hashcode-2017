c30fe562e07e8018fc562853537b334ecd53b932
TN1ck <tomwanick@gmail.com>
2017-02-23 22:13:21 +0100

    more optimizing

--- main.py
+++ main.py
@@ -116,6 +116,7 @@ class CacheServer(object):
         return self.__str__()
 
     def append_endpoint(self, endpoint):
+
         self.endpoints.append(endpoint)
 
     def append_video(self, video):
@@ -164,19 +165,25 @@ def solve(videos, endpoints, requests, cache_servers, cache_server_capacity):
     for cache_server in sorted_cache_servers:
         print('{} {}'.format(cache_server.id, len(cache_servers)), sep=' ', end='', flush=True)
         video_sum = 0
-        video_score = [0 for i in videos]
+        video_score = {}
         for endpoint in cache_server.endpoints:
             for request in endpoint.requests:
                 video = videos[request.video_id]
-                video_score[request.video_id] += (
+                result = (
                     endpoint.latency_to_datacenter_norm - endpoint.get_cache_server_latency_norm(cache_server.id)
                 ) * video.ratio_endpoint_norm(endpoint.id)
-        video_score_enumerated = enumerate(video_score)
+                if request.video_id in video_score:
+                    video_score[request.video_id] += result
+                else:
+                    video_score[request.video_id] = result
+        video_score_enumerated = video_score.items()
         sorted_video_score_enumerated = sorted(video_score_enumerated, key=lambda x: -x[1])
 
         cache_filled_status = 0
         # print(video_score)
         for (index, score) in sorted_video_score_enumerated:
+            if score == 0:
+                break
             video = videos[index]
             if (cache_filled_status + video.size > cache_server.size):
                 continue
